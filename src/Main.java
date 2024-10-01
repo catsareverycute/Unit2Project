ad2794e438c1ff918399efce622ed8b37901cc41
BT_2S14_29 <email@gmail.com>
2024-10-01 11:05:23 -0400

    linear equation constructor works, working on toString()

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String coordinate = "(1,313131313)";
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        String x1 = (coordinate.substring(coordinate.indexOf(",") - 1));
-        String test = (coordinate.substring(coordinate.indexOf(",") + 1));
-        String y1 = test.substring(0,test.length()-1);
-        System.out.println(y1);
+
+        String coordinate1 = "(212,626)";
+        String coordinate2 = "(123,456)";
+
+        String test1 = (coordinate1.substring(coordinate1.indexOf(",") + 1));
+        String test2 = (coordinate2.substring(coordinate2.indexOf(",") + 1));
+        int x1 = Integer.parseInt((coordinate1.substring(1,coordinate1.indexOf(","))));
+        int y1 = Integer.parseInt(test1.substring(0,test1.length()-1));
+        int x2 = Integer.parseInt((coordinate2.substring(1,coordinate2.indexOf(","))));
+        int y2 = Integer.parseInt(test2.substring(0,test2.length()-1));
+        System.out.println("(" + x1 + "," + y1 + ")" + "(" + x2 + "," + y2 + ")");
+
         }
     }
